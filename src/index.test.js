3eae7028a6b2a5c59f1dc04424609399f216ef60
Gabriel Csapo <gabecsapo@gmail.com>
2021-07-14 12:50:33 -0700

    0.0.1 - Basic functionality and tests are provided

---
+++ src/index.test.js
@@ -0,0 +1,81 @@
+const { test, expect } = require("@jest/globals");
+const plugin = require("./index");
+
+describe("docusaurus-plugin-matomo", () => {
+  test("should handle not passing in any options", () => {
+    expect(() => {
+      plugin({ siteConfig: { themeConfig: {} } });
+    }).toThrowErrorMatchingInlineSnapshot(
+      `"You need to specify options arguments use docusaurus-plugin-matomo."`
+    );
+  });
+
+  test("should be able to render out the right header information given the base matomo options", () => {
+    process.env.NODE_ENV = "production";
+
+    expect(
+      plugin(
+        {
+          siteConfig: {
+            themeConfig: {},
+          },
+        },
+        {
+          siteId: "TEST_SITE_ID",
+          matomoUrl: "TEST_MATOMO_URL",
+          siteUrl: "TEST_SITE_URL",
+        }
+      ).injectHtmlTags()
+    ).toMatchInlineSnapshot(`
+Object {
+  "headTags": Array [
+    Object {
+      "attributes": Object {
+        "href": "TEST_MATOMO_URL/piwik.js",
+        "rel": "preconnect",
+      },
+      "tagName": "link",
+    },
+    Object {
+      "innerHTML": "
+            var img = document.createElement('img');
+            img.src = \\"TEST_MATOMO_URL/piwik.php?idsite=TEST_SITE_ID&rec=1&url=TEST_SITE_URL\\" + location.pathname;
+            img.style = \\"border:0\\";
+            img.alt = \\"tracker\\";
+
+            var s = document.getElementsByTagName('script')[0];
+            s.parentNode.insertBefore(img,s);
+            ",
+      "tagName": "noscript",
+    },
+    Object {
+      "innerHTML": "
+            window.dev = undefined
+            if (window.dev === true || !(navigator.doNotTrack === '1' || window.doNotTrack === '1')) {
+              window._paq = window._paq || [];
+              
+              
+              
+              
+              
+              window._paq.push(['setTrackerUrl', 'TEST_MATOMO_URL/piwik.php']);
+              window._paq.push(['setSiteId', 'TEST_SITE_ID']);
+              window._paq.push(['enableHeartBeatTimer']);
+              window.start = new Date();
+              (function() {
+                var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+                g.type='text/javascript'; g.async=true; g.defer=true; g.src='TEST_MATOMO_URL/piwik.js'; s.parentNode.insertBefore(g,s);
+              })();
+              if (window.dev === true) {
+                console.debug('[Matomo] Tracking initialized')
+                console.debug('[Matomo] matomoUrl: TEST_MATOMO_URL, siteId: TEST_SITE_ID')
+              }
+            }
+            ",
+      "tagName": "script",
+    },
+  ],
+}
+`);
+  });
+});
---
+++ src/piwik.js
@@ -0,0 +1,73 @@
+import ExecutionEnvironment from "@docusaurus/ExecutionEnvironment";
+
+let first = true;
+
+function getDuration() {
+  const start = window.start || new Date();
+  const now = new Date();
+  const difference = now.getTime() - start.getTime();
+
+  if (difference === 0) {
+    return null;
+  }
+
+  return difference;
+}
+
+export default (function () {
+  if (!ExecutionEnvironment.canUseDOM) {
+    return null;
+  }
+
+  return {
+    onRouteUpdate({ location, prevLocation }) {
+      if (process.env.NODE_ENV === "production" || window.dev === true) {
+        if (!window._paq) return;
+
+        const { _paq, dev } = window;
+        const url =
+          location && location.pathname + location.search + location.hash;
+        const prevUrl =
+          prevLocation &&
+          prevLocation.pathname + prevLocation.search + prevLocation.hash;
+
+        const sendPageView = () => {
+          const { title } = document;
+
+          prevUrl && _paq.push(["setReferrerUrl", prevUrl]);
+          _paq.push(["setCustomUrl", url]);
+          _paq.push(["setDocumentTitle", title]);
+          _paq.push(["trackPageView"]);
+          _paq.push(["enableLinkTracking"]);
+          _paq.push(["trackAllContentImpressions"]);
+
+          if (dev) {
+            console.debug(`[Matomo] Page view for: ${url} - ${title}`);
+          }
+        };
+
+        // Minimum delay for reactHelmet's requestAnimationFrame
+        const delay = Math.max(32, 0);
+        setTimeout(sendPageView, delay);
+
+        if (first) {
+          first = false;
+
+          _paq.push([
+            "trackEvent",
+            "javascript",
+            "load",
+            "duration",
+            getDuration(),
+          ]);
+
+          if (dev) {
+            console.debug(`[Matomo] Tracking duration for: ${url}`);
+          }
+        }
+      }
+
+      return null;
+    },
+  };
+})();
